f81b85f13d399df77def8782d62c84c397254e51
Nik Danilov <dev.nik.danilov@gmail.com>
2020-06-15 23:31:37 -0400

    init coding

---
+++ binary-search/binary-search.js
@@ -0,0 +1,35 @@
+/* Typical comparison function */
+let defaultCompare = (a, b) =>
+  a > b ? 1 : (a < b ? -1 : 0);
+
+/* Version 1:
+    O(n)
+    Fixed memory
+    Loops
+*/
+let binarySearchWithLoops = (array, element, compare = defaultCompare) => {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((right + left) / 2);
+
+        switch (compare(element, array[middle])) {
+            case -1: {
+                right = middle - 1;
+                break;
+            }
+            case 1: {
+                left = middle + 1;
+                break;
+            }
+            default: {
+                return middle;
+            }
+        }
+    }
+
+    return -1;
+};
+
+export default binarySearchWithLoops;
